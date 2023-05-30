9dd22cf202806cb266a2f749eca15e23d35b5f00
Robert <rckhzouz@gmail.com>
2023-05-30 10:47:26 -0400

    routing functionality for home page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import './App.css';
 import Header from "./Header.js";
 import Home from "./Home.js";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"; // "Routes" was formerly referred to as "Switch"
 
 function App() {
   return (
     // BEM Convention
-    <div className="app">
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      <div className="app">
+        <Routes>
+          <Route path="/" 
+          element={ <> <Header/> <Home/> </> } 
+          />
+          {/*<Route path="/checkout" 
+          element={ <> <Header/> <h1>I AM THE CHECKOUT PAGE</h1> </> }
+          /> */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
